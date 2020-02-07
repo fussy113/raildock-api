f202a36b02fe0113dc09e7657b447dd658e0ca67
fussy113 <s1220080@gmail.com>
2020-02-07 15:38:26 +0900

    modify

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-compose run --rm app bundle install
-
-docker-compose run --rm app yarn install
-
-docker-compose run --rm app bundle exec rails db:migrate
-
-docker-compose up -d
