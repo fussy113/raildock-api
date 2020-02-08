932b636e0059fc1ac2dcb3148e18b53d654968aa
fussy113 <s1220080@gmail.com>
2020-02-08 15:43:48 +0900

    test sample

--- code/spec/controllers/api/v1/greetings_controller_spec.rb
+++ code/spec/controllers/api/v1/greetings_controller_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Api::V1::GreetingsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+      json = JSON.parse(response.body)
       expect(response).to have_http_status(:success)
+      expect(json['message']).to eq 'Hello, World!'
     end
   end
 
